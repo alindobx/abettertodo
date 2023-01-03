0e640db97b45e328a8709faf43a0a3f9cd3767c9
zitechlo <tonyromebx@gmail.com>
2023-01-03 18:45:08 -0500

    we have completed 3 errors

--- js/index.js
+++ js/index.js
@@ -1,36 +1,63 @@
 //get connections to the DOM
 const createToDoBtn = document.querySelector('.plus-symbol');
 const getLowerDiv = document.querySelector('.module-container');
-
+const countDiv = document.querySelector('.numbOfItems');
+const clearCompletedBtn = document.querySelector('.clear-completed');
 const createDiv = document.createElement('input');
+
+//filters
+//Number of Items
+let todoCount = 0;
 
 //Event Listeners=========================================================
 //=========================================================================
 createToDoBtn.addEventListener('click',(e)=>{
     // Capture the value in the input field
+    e.preventDefault();
     const inputContent = document.getElementById('todo-value').value;
-    //create div first
-    const second = document.createElement("input");
-    second.setAttribute("type","checkbox");
-    second.className = "checkbox-type";
+    todoCount += 1;
+    console.log(todoCount);
+    countDiv.innerHTML = `${todoCount}`
+
+    //create label container element
     const first = document.createElement("label");
     first.setAttribute("for",'input-content');
     first.className = "input-content";
-    const text = document.createTextNode(`${inputContent}`);
 
-    first.appendChild(second)
-    first.appendChild(text);
-    getLowerDiv.append(first);
-    checkBoxEvent();
+    //create input element
+    const second = document.createElement("input");
+    second.setAttribute("type","checkbox");
+    second.className = "checkbox-type";
+    first.append(second);
+    second.insertAdjacentHTML("afterend", inputContent );
 
-    console.log('test');
-    console.log(inputContent);
+    //creating img inside container div
+    const third = document.createElement('img');
+    third.setAttribute('src','img/icon-cross.svg');
+    third.setAttribute('class','deleteBtn');
 
+    //onclick Delete entire div
+    third.onclick = (e) => {
+        getLowerDiv.removeChild(e.target.parentElement);
+        todoCount -= 1;
+        countDiv.innerHTML = `${todoCount}`
+    }
+    //img button inside label div
+    first.append(third);
+
+    //appending as Child to the main div container
+    getLowerDiv.appendChild(first);
+
+    //remove the content from input field on click
+    const newContent = document.getElementById('todo-value');
+    newContent.value = '';
+
+    //strike the content value when checkbox is checked
+    checkBoxEvent()
 })
-
+// add a checkmark to checkbox
 const checkBoxEvent = () => {
     const getLabel = document.querySelectorAll('.checkbox-type');
-    console.log(getLabel)
         getLabel.forEach(item =>{
             item.addEventListener('change',(e)=>{
                 let selectedTask = e.target;
@@ -46,3 +73,27 @@ const checkBoxEvent = () => {
         })
     }
 checkBoxEvent();
+
+//clear completed
+clearCompletedBtn.addEventListener('click',()=>{
+    // gather all the divs that have been struck completed - strike
+    let gatherClear = document.querySelectorAll('.strike');
+
+    // Keep track of how many labels have been struck
+    todoCount = todoCount - gatherClear.length;
+
+    // making sure number doesn't go below 0
+    todoCount < 0 ? countDiv.innerHTML = "0" : countDiv.innerHTML = `${todoCount}`
+    console.log(todoCount)
+    gatherClear.forEach(clear =>{
+        clear.style.display = "none";
+    })
+
+    //remove node list elements
+    for(let elem of gatherClear) {
+        elem.parentNode.removeChild(elem);
+    }
+
+})
+
+
